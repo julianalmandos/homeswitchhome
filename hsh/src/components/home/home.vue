675c5d9b1a948cf1577d75e222b9ce642771cdd8
Oriana Arevalos <orianarevalos@gmail.com>
2019-05-15 23:44:55 -0300

    Actualizando el home respecto al editar

--- hsh/src/components/home/home.vue
+++ hsh/src/components/home/home.vue
@@ -1,30 +1,17 @@
 <template>
   <div class="home">
-    <h1>Propiedades</h1>
-    <b-button v-b-modal.editPropertyModal>Editar</b-button>
-
-    <editProperty :property="dataRegisterProperty"/>
+    <h1>Propiedades</h1> 
     <listOfProperties/>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import editProperty from '@/components/editProperty/editProperty.vue'
+// @ is an alias to /src 
 import listOfProperties from '@/components/listOfProperties/listOfProperties.vue'
 export default {
   name: 'home',
   components: {
-     listOfProperties,
-     editProperty
-  },
-  data() {
-    return {
-      dataRegisterProperty: {
-        id: 1,
-        description: 'hola',
-      }
-    }
-  }
+     listOfProperties, 
+  }, 
 }
 </script>
