6c13ef5af971f011675317747127a84c463481da
unknown <julianalmandos@gmail.com>
2019-05-19 05:47:47 -0300

    Corrección de borrado de propiedades

--- hsh/src/components/administrationPanel/listOfProperties.vue
+++ hsh/src/components/administrationPanel/listOfProperties.vue
@@ -1,7 +1,7 @@
 <template>
     <b-container>
         <h1 class="titulo">Lista de Propiedades</h1><br>
-        <propertyCard v-for="property in properties" :key="property.id" :property="property"/>
+        <propertyCard v-for="property in properties" :key="property.id" :property="property" @deleted="reloadProperties"/>
     </b-container>
 </template>
 
@@ -30,7 +30,16 @@
                 })
         },
         methods: {
+            reloadProperties() {
+                axios.get('//localhost:3000/properties')
+                    .then(response => {
+                        console.log('anda');
+                        this.properties=response.data;
+                    })
+                    .catch(error => {
 
+                    })
+            }
         }
     }
 
