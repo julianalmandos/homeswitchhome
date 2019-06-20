aa251ec08d5765ed93b5ea284ffd8265336dadd7
unknown <julianalmandos@gmail.com>
2019-06-20 18:40:56 -0300

    Fixes

--- hsh_server/app.js
+++ hsh_server/app.js
@@ -124,7 +124,7 @@ app.post('/closeAuction/:id', (req, res) => {
       console.log(result);
       if(result[0]==null){
         console.log(req.body.data);
-        sql = "INSERT INTO users (email,password,name,surname,birthday,card_number,card_expiration_month,card_expiration_year,card_security_number) VALUES ('" + req.body.data.email + "','" + contraseña + "','" + req.body.data.name + "','" + req.body.data.surname + "','" + new Date(req.body.data.birthday).toLocaleDateString() + "','" + req.body.data.cardNumber + "','" + req.body.data.cardExpirationDate.month + "','" + req.body.data.cardExpirationDate.year + "','" + req.body.data.cardSecurityNumber + "')";
+        sql = "INSERT INTO users (email,password,name,surname,birthday,card_type,card_number,card_expiration_month,card_expiration_year,card_security_number) VALUES ('" + req.body.data.email + "','" + contraseña + "','" + req.body.data.name + "','" + req.body.data.surname + "','" + new Date(req.body.data.birthday).toLocaleDateString() + "','" + req.body.data.cardType + "','" + req.body.data.cardNumber + "','" + req.body.data.cardExpirationDate.month + "','" + req.body.data.cardExpirationDate.year + "','" + req.body.data.cardSecurityNumber + "')";
         console.log(sql);
         conn.query(sql, function (err, result) {
           if (err) throw err;
