fdd6ced7ccc5e7df1a78dc75518c5cf876749fc6
cande_8998@hotmail.com <cande_8998@hotmail.com>
2019-06-21 15:03:36 -0300

    listo

--- hsh/src/components/toolbar/toolbar.vue
+++ hsh/src/components/toolbar/toolbar.vue
@@ -13,23 +13,6 @@
           <b-nav-item class="navLink" to="/"><strong><font-awesome-icon icon="home"></font-awesome-icon> Inicio</strong></b-nav-item>
           <b-nav-item v-if="user!=null" class="navLink" to="/auctions"><strong><font-awesome-icon icon="gavel"></font-awesome-icon> Subastas</strong></b-nav-item>
         </b-navbar-nav>
-       <!-- <b-navbar-nav>
-          <b-nav-form>
-            <b-input-group>
-              <b-form-input size="sm" style="width:300px;" placeholder="Escriba una localidad" required></b-form-input>
-              <b-form-input size="sm" class="mr-sm-2 mb-sm-3"
-                        id="range"
-                        type="date"
-                        required
-                        placeholder="Ingrese un rango"
-                        style="width:200px"
-                    ></b-form-input>
-              <b-input-group-append>
-                <b-button size="sm" class="my-2 my-sm-0 blueButton" :to="{ name: 'searchProperties', params: { locality: this, range:this }}" type="submit">Buscar</b-button>
-              </b-input-group-append>              
-            </b-input-group>
-          </b-nav-form>
-        </b-navbar-nav> -->
 
         <!-- Right aligned nav items -->
         <b-navbar-nav class="ml-auto">
@@ -143,14 +126,15 @@
         this.$refs.dropdown.hide();
       },
       check(){
-
+        var actualDate = new Date()
+        actualDate= actualDate.toISOString().substring(0,10);
         var controlDate= new Date(this.startDate.substring(0,4),this.startDate.substring(5,7),this.startDate.substring(8,10));
         controlDate.setDate(controlDate.getDate()+60);
         controlDate = controlDate.toISOString().substring(0,10);
         console.log(this.startDate);
         console.log(this.finishDate);
         if(this.locality !== '' && this.startDate !== '' && this.finishDate !== ''){
-          if ((this.startDate>this.finishDate)||(controlDate<=this.finishDate)){
+          if ((this.startDate>this.finishDate)||(controlDate<=this.finishDate)||(this.startDate<actualDate)){
             this.showErrorWrongDates = true
           }
         }else{
