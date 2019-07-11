f6f85916b6703c3dd409dd98cd11946620cf202a
Julian Almandos <julianalmandos@gmail.com>
2019-07-11 14:34:32 -0300

    Terminado el modificar precios de suscripción

--- hsh/src/components/administrationPanel/suscriptionPrices.vue
+++ hsh/src/components/administrationPanel/suscriptionPrices.vue
@@ -2,9 +2,20 @@
     <b-container>
         <h1 class="titulo">Precios de Suscripción</h1>
         <br>
-        <form @submit.stop.prevent="changeSuscriptionPrices()">
-
-        </form>
+        <div class="formulario">
+            <form @submit.stop.prevent="changeSuscriptionPrices()" style="width:30%">
+                <label for="normalPrice">Precios Usuario Normal</label>
+                <b-form-group id="normalPrice">
+                    <b-form-input type="number" v-model="prices[0].price" required/>
+                </b-form-group>
+                <label for="premiumPrice">Precios Usuario Normal</label>
+                <b-form-group id="premiumPrice">
+                    <b-form-input type="number" v-model="prices[1].price" required></b-form-input>
+                </b-form-group>
+                <br>  
+                <b-button type="submit" class="blueButton">Modificar!</b-button>
+            </form>
+        </div>
     </b-container>
 </template>
 
@@ -28,10 +39,18 @@
             changeSuscriptionPrices(){
                 if(confirm('¿Está seguro que desea modificar los precios de suscripción?')){
                     axios.post('//localhost:3000/suscriptionPrices',{
-                        data: prices,
+                        data: {
+                            normalPrice: this.prices[0].price,
+                            premiumPrice: this.prices[1].price,
+                        },
                     })
                     .then(response => {
-                        //toast y recargar precios
+                        this.$bvToast.toast('Los precios de las suscripciones fueron modificados con éxito!', {
+                            title: 'Modificación exitosa!',
+                            variant: 'success',
+                            autoHideDelay: 5000,
+                            toaster: 'b-toaster-bottom-right',
+                        });
                         this.reloadPrices();
                     })
                     .catch(error => {
@@ -49,4 +68,15 @@
     }
 
 </script>
+
+<style scoped>
+
+    .formulario {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+
+</style>
+
 
