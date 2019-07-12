a2d330eb659af85d467e166719c6bee15607c636
cande_8998@hotmail.com <cande_8998@hotmail.com>
2019-07-12 16:58:44 -0300

    correccion

--- hsh/src/components/administrationPanel/administrationPanel.vue
+++ hsh/src/components/administrationPanel/administrationPanel.vue
@@ -50,13 +50,13 @@
             <font-awesome-icon class="fa-5x" icon="gavel"></font-awesome-icon>
             <b-card-text style="font-size:25px">Cerrar subastas</b-card-text>
         </b-card>
+      </b-card-group>
+      <br>
+      <b-card-group deck>
         <b-card class="tarjeta text-center" @click="openUsersList" bg-variant="light" >
             <font-awesome-icon class="fa-5x" icon="users"></font-awesome-icon>
             <b-card-text style="font-size:25px">Ver listado de usuarios</b-card-text>
         </b-card>
-      </b-card-group>
-      <br>
-      <b-card-group deck>
         <b-card class="tarjeta text-center" @click="openSuscriptionPrices" bg-variant="light">
             <font-awesome-icon class="fa-5x" icon="dollar-sign"></font-awesome-icon>
             <b-card-text style="font-size:25px">Precios de Suscripción</b-card-text>
