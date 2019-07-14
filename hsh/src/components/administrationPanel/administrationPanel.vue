6103d1c8e627428bada3ac209b78c11b4389057c
Oriana Arevalos <orianarevalos@gmail.com>
2019-07-14 19:13:51 -0300

    arreglando el abrir subasta

--- hsh/src/components/administrationPanel/administrationPanel.vue
+++ hsh/src/components/administrationPanel/administrationPanel.vue
@@ -20,20 +20,20 @@
       <br>
       <b-card-group deck>
         <b-card class="tarjeta text-center" @click="openNormalRequests" bg-variant="light">
-            <font-awesome-icon class="fa-5x" icon="star"></font-awesome-icon>
-            <b-card-text style="font-size:25px">Solicitudes Normales</b-card-text>
+          <font-awesome-icon class="fa-5x" icon="star"></font-awesome-icon>
+          <b-card-text style="font-size:25px">Solicitudes Normales</b-card-text>
         </b-card>
         <b-card class="tarjeta text-center" @click="openPremiumRequests" bg-variant="light">
-            <font-awesome-icon class="fa-5x" icon="star" style="color:#e6e600;"></font-awesome-icon>
-            <b-card-text style="font-size:25px">Solicitudes Premium</b-card-text>
+          <font-awesome-icon class="fa-5x" icon="star" style="color:#e6e600;"></font-awesome-icon>
+          <b-card-text style="font-size:25px">Solicitudes Premium</b-card-text>
         </b-card>
-       <!-- <b-card class="tarjeta text-center" @click="chargeSubscription" bg-variant="light">
+        <!-- <b-card class="tarjeta text-center" @click="chargeSubscription" bg-variant="light">
             <font-awesome-icon class="fa-5x" icon="money-check"></font-awesome-icon>
             <b-card-text style="font-size:25px">Cobrar suscripcion</b-card-text>
-        </b-card> --> 
+        </b-card>-->
         <b-card class="tarjeta text-center" @click="openLocationOptions" bg-variant="light">
-            <font-awesome-icon class="fa-5x" icon="globe-americas"></font-awesome-icon>
-            <b-card-text style="font-size:25px">Agregar ubicación</b-card-text>
+          <font-awesome-icon class="fa-5x" icon="globe-americas"></font-awesome-icon>
+          <b-card-text style="font-size:25px">Agregar ubicación</b-card-text>
         </b-card>
       </b-card-group>
       <br>
@@ -46,20 +46,20 @@
           <font-awesome-icon class="fa-5x" icon="gavel"></font-awesome-icon>
           <b-card-text style="font-size:25px">Abrir subastas</b-card-text>
         </b-card>
-        <b-card class="tarjeta text-center" @click="closeAuction" bg-variant="light" >
-            <font-awesome-icon class="fa-5x" icon="gavel"></font-awesome-icon>
-            <b-card-text style="font-size:25px">Cerrar subastas</b-card-text>
+        <b-card class="tarjeta text-center" @click="closeAuction" bg-variant="light">
+          <font-awesome-icon class="fa-5x" icon="gavel"></font-awesome-icon>
+          <b-card-text style="font-size:25px">Cerrar subastas</b-card-text>
         </b-card>
       </b-card-group>
       <br>
       <b-card-group deck>
-        <b-card class="tarjeta text-center" @click="openUsersList" bg-variant="light" >
-            <font-awesome-icon class="fa-5x" icon="users"></font-awesome-icon>
-            <b-card-text style="font-size:25px">Ver listado de usuarios</b-card-text>
+        <b-card class="tarjeta text-center" @click="openUsersList" bg-variant="light">
+          <font-awesome-icon class="fa-5x" icon="users"></font-awesome-icon>
+          <b-card-text style="font-size:25px">Ver listado de usuarios</b-card-text>
         </b-card>
         <b-card class="tarjeta text-center" @click="openSuscriptionPrices" bg-variant="light">
-            <font-awesome-icon class="fa-5x" icon="dollar-sign"></font-awesome-icon>
-            <b-card-text style="font-size:25px">Precios de Suscripción</b-card-text>
+          <font-awesome-icon class="fa-5x" icon="dollar-sign"></font-awesome-icon>
+          <b-card-text style="font-size:25px">Precios de Suscripción</b-card-text>
         </b-card>
       </b-card-group>
     </b-container>
@@ -77,65 +77,59 @@ export default {
     openBookingList() {
       this.$router.push("/panel/bookings");
     },
-    openUsersList(){
-      this.$router.push('/panel/users');
+    openUsersList() {
+      this.$router.push("/panel/users");
     },
     openPublishProperty() {
-      this.$router.push('/properties/publish');
+      this.$router.push("/properties/publish");
     },
     openNormalRequests() {
-      this.$router.push('/panel/normal_requests');
+      this.$router.push("/panel/normal_requests");
     },
     openPremiumRequests() {
-      this.$router.push('/panel/premium_requests');
+      this.$router.push("/panel/premium_requests");
     },
-    closeAuction(){
+    closeAuction() {
       axios
-          .get("http://localhost:3000/closeAuctions")
-          .then(response => {
-            this.$bvToast.toast(
-              `Se cerraron ${response.data.length} subastas`,
-              {
-                title: "Operación Exitosa",
-                variant: "success",
-                autoHideDelay: 5000,
-                toaster: "b-toaster-bottom-right"
-              }
-            );
-            this.selectWinner(response.data)
-          })
-          .catch(error => {
-            console.error(error);
+        .get("http://localhost:3000/closeAuctions")
+        .then(response => {
+          this.$bvToast.toast(`Se cerraron ${response.data.length} subastas`, {
+            title: "Operación Exitosa",
+            variant: "success",
+            autoHideDelay: 5000,
+            toaster: "b-toaster-bottom-right"
           });
-    },
-    selectWinner(closed){
-      closed.forEach(element => {
-        axios.post("http://localhost:3000/selectWinner",{
-          data: {
-              week: element
-          }
+          this.selectWinner(response.data);
         })
-      })
+        .catch(error => {
+          console.error(error);
+        });
+    },
+    selectWinner(closed) {
+      closed.forEach(element => {
+        axios.post("http://localhost:3000/selectWinner", {
+          data: {
+            week: element
+          }
+        });
+      });
     },
     openLocationOptions() {
-      this.$router.push('/panel/location_options');
+      this.$router.push("/panel/location_options");
     },
     openSuscriptionPrices() {
-      this.$router.push('/panel/suscription_prices');
+      this.$router.push("/panel/suscription_prices");
     },
     generateWeeks() {
       axios
         .get("http://localhost:3000/properties/generateWeeks")
         .then(response => {
-          this.$bvToast.toast(
-            `Las semanas fueron generadas exitosamente.`,
-            {
-              title: "Operación exitosa!",
-              variant: "success",
-              autoHideDelay: 5000,
-              toaster: "b-toaster-bottom-right"
-            }
-          );
+          this.$bvToast.toast(`Las semanas fueron generadas exitosamente.`, {
+            title: "Operación exitosa!",
+            variant: "success",
+            autoHideDelay: 5000,
+            toaster: "b-toaster-bottom-right"
+          });
         })
         .catch(error => {
           console.error(error);
@@ -145,53 +139,55 @@ export default {
       axios
         .get("http://localhost:3000/properties/openAuctions")
         .then(response => {
-          this.$bvToast.toast(
-            `Las subastas fueron abiertas exitosamente.`,
-            {
+          if (response.data == 0) {
+            this.$bvToast.toast(`No hay subastas por abrir`, {
+              title: "Operación fallida!",
+              variant: "danger",
+              autoHideDelay: 5000,
+              toaster: "b-toaster-bottom-right"
+            });
+          } else {
+            this.$bvToast.toast(`Las subastas fueron abiertas exitosamente.Se abrieron ${response.data} subastas`, {
               title: "Operación exitosa!",
               variant: "success",
               autoHideDelay: 5000,
               toaster: "b-toaster-bottom-right"
-            }
-          );
-        })
-        .catch(error => {
-          console.error(error);
-        });
-    },
-    chargeSubscription(){
-      axios
-        .get("http://localhost:3000/chargeSubscription")
-        .then(response => {
-          if (response.data.data>0){
-          this.$bvToast.toast(
-            `Se cobró la suscripción: $`+ response.data.data,
-            {
-              title: "Operación Exitosa",
-              variant: "success",
-              autoHideDelay: 5000,
-              toaster: "b-toaster-bottom-right"
-            }
-          );
-          }else {
-            this.$bvToast.toast(
-            `Ya se cobró la suscripción este mes`,
-            {
-              title: "Operación Fallida",
-              variant: "danger",
-              autoHideDelay: 5000,
-              toaster: "b-toaster-bottom-right"
-            }
-          );
+            });
           }
         })
         .catch(error => {
           console.error(error);
         });
-      
+    },
+    chargeSubscription() {
+      axios
+        .get("http://localhost:3000/chargeSubscription")
+        .then(response => {
+          if (response.data.data > 0) {
+            this.$bvToast.toast(
+              `Se cobró la suscripción: $` + response.data.data,
+              {
+                title: "Operación Exitosa",
+                variant: "success",
+                autoHideDelay: 5000,
+                toaster: "b-toaster-bottom-right"
+              }
+            );
+          } else {
+            this.$bvToast.toast(`Ya se cobró la suscripción este mes`, {
+              title: "Operación Fallida",
+              variant: "danger",
+              autoHideDelay: 5000,
+              toaster: "b-toaster-bottom-right"
+            });
+          }
+        })
+        .catch(error => {
+          console.error(error);
+        });
     }
   }
-}
+};
 </script>
 
 <style scoped>
