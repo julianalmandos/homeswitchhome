72de4c1e2a08f789503b9c4eec08971ab7bb6a55
Oriana Arevalos <orianarevalos@gmail.com>
2019-07-14 16:01:06 -0300

    Funciona el abrir subasta

--- hsh_server/models/properties.js
+++ hsh_server/models/properties.js
@@ -24,65 +24,60 @@ app.get('/generateWeeks', (req, res) => {
     result.forEach(prop => {
       var fechaInicial = new Date();
       const sqlWeeks = `SELECT * FROM weeks WHERE idProperty=${prop.id} AND date >= "${fechaInicial.toISOString().substring(0, 10)}"`;
-      conn.query(sqlWeeks, function (err, result) { 
-        if (result.length == 0) { 
+      conn.query(sqlWeeks, function (err, result) {
+        if (result.length == 0) {
           totalWeeks += 53;
           while (fechaInicial.getDay() != 0) {
             fechaInicial.setDate(fechaInicial.getDate() + 1);
           }
           for (var i = 0; i < 53; i++) {
-            var sql2 = "INSERT INTO weeks (idProperty,date,auction,reserved,idle,auctionDate) VALUES (" + prop.id + ",'" + fechaInicial.toISOString().substring(0, 10) + "',0,0,0,'"+new Date(0000,00,00).toISOString().substring(0,10)+"')";
+            var sql2 = "INSERT INTO weeks (idProperty,date,auction,reserved,idle,auctionDate) VALUES (" + prop.id + ",'" + fechaInicial.toISOString().substring(0, 10) + "',0,0,0,'" + new Date(0000, 00, 00).toISOString().substring(0, 10) + "')";
             conn.query(sql2, function (err, result) {
               if (err) throw err;
             });
             fechaInicial.setDate(fechaInicial.getDate() + 7);
           }
-        } else if (result.length < 53) { 
+        } else if (result.length < 53) {
           max = new Date();
           const quantityWeeks = 53 - result.length;
           totalWeeks += quantityWeeks;
           for (var i = 0; i < result.length; i++) {
             console.log(result[i].date)
-            if (result[i].date > max) { 
+            if (result[i].date > max) {
               max = result[i].date;
             }
           }
           console.log("El maximo es ", max);
           for (var i = 0; i < quantityWeeks; i++) {
-            max.setDate(max.getDate()+7)
-            var sql2 = "INSERT INTO weeks (idProperty,date,auction,reserved,idle,auctionDate) VALUES (" + prop.id + ",'" + max.toISOString().substring(0, 10) + "',0,0,0,'"+new Date(0000,00,00).toISOString().substring(0,10)+"')";
+            max.setDate(max.getDate() + 7)
+            var sql2 = "INSERT INTO weeks (idProperty,date,auction,reserved,idle,auctionDate) VALUES (" + prop.id + ",'" + max.toISOString().substring(0, 10) + "',0,0,0,'" + new Date(0000, 00, 00).toISOString().substring(0, 10) + "')";
             conn.query(sql2, function (err, result) {
               if (err) throw err;
             });
-          } 
+          }
         };
-       });
-      })
-res.send(`${totalWeeks}`)
+      });
+    })
+    res.send(`${totalWeeks}`)
   });
 });
 
 app.get('/openAuctions', (req, res) => {
-  const sql = `SELECT id FROM properties`;
-  conn.query(sql, function (err, result) {
-    var actualDate = new Date();
-    totalAuctions = 0;
-    result.forEach(function (prop) {
-      const sqlWeeks = `SELECT * FROM weeks WHERE idProperty=${prop.id} AND auction=0`;
-      conn.query(sqlWeeks, function (err, result) {
-        result.forEach(function (week) {
-          week.date.setMonth(week.date.getMonth() - 6);
-          if (actualDate > week.date) {
-            totalAuctions += 1;
-            const sql = `UPDATE weeks w SET w.auction = 1, w.auctionDate="${actualDate.toISOString().substring(0, 10)}" WHERE id=${week.id}`
-            conn.query(sql, function (err, result) {
-              if (err) throw err;
-            })
-          }
-        })
+  var finishDate = new Date();
+  finishDate.setMonth(finishDate.getMonth() + 6);
+  var startDate = new Date();
+  startDate.setMonth(startDate.getMonth() + 6);
+  startDate.setDate(startDate.getDate() - 3); 
+  const sqlWeeks = `SELECT * FROM weeks WHERE auction=0 AND (date BETWEEN "${startDate.toISOString().substring(0, 10)}" AND "${finishDate.toISOString().substring(0, 10)}")`;
+  conn.query(sqlWeeks, function (err, result) {
+    result.forEach(function (week) {
+      var actualDate = new Date();
+      const sql = `UPDATE weeks w SET w.auction = 1, w.auctionDate="${actualDate.toISOString().substring(0, 10)}" WHERE id=${week.id}`
+      conn.query(sql, function (err, result) {
+        if (err) throw err;
       })
     })
-    res.send(`${totalAuctions}`)
+    res.send(`${result.length}`)
   })
 })
 
@@ -121,9 +116,9 @@ app.get('/:id/bookings', function (req, res) {
 })
 
 app.post('/', function (req, res) {
-  var sql = "SELECT * FROM properties p WHERE p.locality='"+req.body.data.locality+"' AND p.province='"+req.body.data.province+"' AND p.name='"+req.body.data.name+"'";
+  var sql = "SELECT * FROM properties p WHERE p.locality='" + req.body.data.locality + "' AND p.province='" + req.body.data.province + "' AND p.name='" + req.body.data.name + "'";
   conn.query(sql, function (err, result) {
-    if(result.length == 0){
+    if (result.length == 0) {
       console.log(req.body.data)
       sql = "INSERT INTO properties (name,description,address,base_price,country,province,locality) VALUES ('" + req.body.data.name + "','" + req.body.data.description + "','" + req.body.data.address + "','" + req.body.data.base_price + "','" + req.body.data.country + "','" + req.body.data.province + "','" + req.body.data.locality + "')";
       conn.query(sql, function (err, result) {
@@ -141,17 +136,17 @@ app.post('/', function (req, res) {
         })
         res.send(result)
       });
-    }else{
+    } else {
       res.sendStatus(409);
     }
-    
+
   })
 })
 
 app.post('/:id/edit', function (req, res) {
-  var sql="SELECT * FROM properties p WHERE p.locality='"+req.body.data.property.locality+"' AND p.province='"+req.body.data.property.province+"' AND p.name='"+req.body.data.property.name+"'";
+  var sql = "SELECT * FROM properties p WHERE p.locality='" + req.body.data.property.locality + "' AND p.province='" + req.body.data.property.province + "' AND p.name='" + req.body.data.property.name + "'";
   conn.query(sql, function (err, result) {
-    if(result.length==0 || (result.length==1 && result[0].id==req.body.data.property.id)){
+    if (result.length == 0 || (result.length == 1 && result[0].id == req.body.data.property.id)) {
       console.log("acata")
       if (req.body.data.property.description !== "" && req.body.data.property.description !== undefined) {
         var sql = "UPDATE properties p SET p.description = '" + req.body.data.property.description + "',p.name = '" + req.body.data.property.name + "',p.base_price = '" + req.body.data.property.base_price + "',p.locality = '" + req.body.data.property.locality + "',p.country = '" + req.body.data.property.country + "',p.province= '" + req.body.data.property.province + "'  WHERE p.id='" + req.params.id + "'";
@@ -175,7 +170,7 @@ app.post('/:id/edit', function (req, res) {
         })
       }
       res.sendStatus(200);
-    }else{
+    } else {
       res.sendStatus(409);
     }
   });
